234294048e851f2e02312d315b924bed4e9e8fe0
Andrew Sayman <asayman@hirezstudios.com>
2024-01-25 17:57:54 -0500

    Test using SIC and A2S at the same time
    # Conflicts:
    #	CMakeLists.txt

--- test/complete/test_sic.h
+++ test/complete/test_sic.h
@@ -16,8 +16,78 @@ limitations under the License.
 #pragma once
 
 #include "lest/lest.hpp"
+#include "configuration.h"
 #include <list>
+
+#include "rh_string.h"
+#include "rh_vector.h"
+
 
 /** @file test_sic.h
  * Tests GameInstanceAdapter in SIC mode
  */
+
+
+namespace test
+{
+namespace sic
+{
+
+    // Helper function to join a vector of strings into a single string.
+    inline rallyhere::string join(const rallyhere::vector<rallyhere::string>& strings, const rallyhere::string& separator)
+    {
+        rallyhere::string result;
+        for (auto& s : strings)
+        {
+            if (!result.empty())
+            {
+                result += separator;
+            }
+            result += s;
+        }
+        return result;
+    }
+
+    template<typename T>
+    struct TestArguments
+    {
+        rallyhere::vector<T> arguments;
+        rallyhere::string arguments_str;
+
+        TestArguments()
+        {
+            get_default_arguments();
+        }
+
+        void get_default_arguments()
+        {
+            arguments = {
+                "rhbootstrapmode=SIC",
+                get_credentials_file_path_arg(),
+                get_rally_here_url_arg(),
+                get_rh_credentials_as_arg(),
+                "A2SPort=23891",
+                "rhsicprofileid=unknownprofileid",
+                "rhsicserverid=unknownserverid",
+                "rhsichostname=unknownhostname",
+                "PoRt=9000",
+                "MULTIHOME=0.0.0.0",
+                "rhsicid=sic_unknownserverid_9000",
+                "rhsicgrouptags=profile_id:unknownprofileid,server_id:unknownserverid,hostname:unknownhostname",
+                "rhsicprometheusbind=0.0.0.0:23890"
+            };
+            arguments_str = join(arguments, " ");
+        }
+
+        const char* c_str()
+        {
+            return arguments_str.c_str();
+        }
+
+        size_t size()
+        {
+            return arguments_str.size();
+        }
+    };
+} // namespace sic
+} // namespace test
