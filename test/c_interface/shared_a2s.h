9786ea12f83ae7f76b8a7decae35a25e40e6aacf
Andrew Sayman <asayman@hirezstudios.com>
2024-02-06 16:40:49 -0500

    Test more of the header values for A2S_INFO

---
+++ test/c_interface/shared_a2s.h
@@ -0,0 +1,25 @@
+/*
+Copyright 2023 RallyHere
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+
+#include "c_api.h"
+#include "shared_test_data.h"
+
+#include "lest/lest.hpp"
+#include "a2s.hpp"
+
+void validate_server_info(lest::env& lest_env, const rallyhere::server_info& info);
+rallyhere::server_info get_stats(lest::env& lest_env, RallyHereGameInstanceAdapterPtr adapter, TestCCodeData& data);
