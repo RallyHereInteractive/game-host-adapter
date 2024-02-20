5e661c7db4f8d8ad3d622aa9f9a2118d55bf45c7
Andrew Sayman <asayman@hirezstudios.com>
2024-02-20 17:57:22 -0500

    Support A2S challenge requests on the testing side

--- test/c_interface/shared_a2s.cpp
+++ test/c_interface/shared_a2s.cpp
@@ -36,6 +36,36 @@ void validate_server_info(lest::env& lest_env, const rallyhere::server_info& inf
     EXPECT(info.EDF == 0);
 }
 
+struct a2s_response_handler
+{
+    lest::env& lest_env;
+    boost::array<uint8_t, rallyhere::A2S_MAX_DATA_SIZE + rallyhere::UDP_HEADER_SIZE> recv_buf{};
+    bool received{ false };
+    rallyhere::a2s_simple_response response{};
+    rallyhere::a2s_challenge_response challenge{};
+    rallyhere::server_info info{};
+
+    void operator()(const boost::system::error_code& ec, size_t len)
+    {
+        received = true;
+        rallyhere::A2SDatagram datagram{ recv_buf.data(), len };
+        datagram >> response;
+        datagram.seek(0);
+        if (response.type == rallyhere::a2s_response_type::challenge)
+        {
+            datagram >> challenge;
+        }
+        else if (response.type == rallyhere::a2s_response_type::info)
+        {
+            datagram >> info;
+        }
+        else
+        {
+            EXPECT(response.type == rallyhere::a2s_response_type::info);
+        }
+    }
+};
+
 rallyhere::server_info get_stats(lest::env& lest_env, RallyHereGameInstanceAdapterPtr adapter, TestCCodeData& data)
 {
     boost::asio::io_context io_context;
@@ -49,19 +79,12 @@ rallyhere::server_info get_stats(lest::env& lest_env, RallyHereGameInstanceAdapt
     std::string_view send_buf = { "\xff\xff\xff\xff\x54Source Engine Query\0", 25 };
     socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
 
-    boost::array<uint8_t, rallyhere::A2S_MAX_DATA_SIZE + rallyhere::UDP_HEADER_SIZE> recv_buf;
+    a2s_response_handler handler{ lest_env };
     udp::endpoint sender_endpoint;
-    bool received{ false };
-    rallyhere::server_info info{};
-    socket.async_receive_from(
-        boost::asio::buffer(recv_buf), sender_endpoint, [&received, &recv_buf, &info](const boost::system::error_code& ec, size_t len) {
-            received = true;
-            rallyhere::A2SDatagram datagram{ recv_buf.data(), len };
-            datagram >> info;
-        });
+    socket.async_receive_from(boost::asio::buffer(handler.recv_buf), sender_endpoint, std::ref(handler));
 
     auto start = std::chrono::steady_clock::now();
-    while (!received)
+    while (!handler.received)
     {
         EXPECT(rallyhere_tick(adapter) == RH_STATUS_OK);
         io_context.poll();
@@ -70,6 +93,27 @@ rallyhere::server_info get_stats(lest::env& lest_env, RallyHereGameInstanceAdapt
         EXPECT(elapsed < std::chrono::seconds(10 + 2));
     }
 
-    validate_server_info(lest_env, info);
-    return info;
+    if (handler.challenge.challenge != 0)
+    {
+        rallyhere::string send_buf = "\xff\xff\xff\xff\x54Source Engine Query\0\xff\xff\xff\xff";
+        rallyhere::A2SDatagram datagram{ send_buf };
+        datagram.seek(25);
+        datagram << handler.challenge.challenge;
+
+        udp::endpoint sender_endpoint;
+        socket.async_receive_from(boost::asio::buffer(handler.recv_buf), sender_endpoint, std::ref(handler));
+
+        start = std::chrono::steady_clock::now();
+        while (!handler.received)
+        {
+            EXPECT(rallyhere_tick(adapter) == RH_STATUS_OK);
+            io_context.poll();
+            auto ongoing = std::chrono::steady_clock::now();
+            auto elapsed = ongoing - start;
+            EXPECT(elapsed < std::chrono::seconds(10 + 2));
+        }
+    }
+
+    validate_server_info(lest_env, handler.info);
+    return handler.info;
 }
